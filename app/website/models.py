566ea78a0f1b0862ebdf3b3f62ac097c4b23831e
Jaidon Froggatt-Morton <jaidonfroggatt@gmail.com>
2025-03-18 18:29:13 +0000

    modified db schema to create one-to-one relationship between cv table and cvdata table

--- app/website/models.py
+++ app/website/models.py
@@ -7,15 +7,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstname = db.Column(db.String(150))
+    cvs = db.relationship('CV')
+    analyses = db.relationship('CVData')
 
 class CV(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     cv = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    cv_data = db.relationship('CVData', uselist=False, back_populates='cv', cascade='all, delete-orphan')
 
 class CVData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cv_id = db.Column(db.Integer, db.ForeignKey('cv.id'))
+    cv_id = db.Column(db.Integer, db.ForeignKey('cv.id'), unique=True)
     data = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    cv = db.relationship('CV', back_populates='cv_data')
